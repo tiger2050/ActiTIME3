133967272656cc8b7a4c068a34596bd5d17fde84
fzkha <fzkha@FZK-PC>
2020-11-02 19:55:58 -0400

    comit done on Oct 2nd

---
+++ src/test/java/jenkinspackage/JenkinsPgm.java
@@ -0,0 +1,25 @@
+package jenkinspackage;
+
+import org.testng.annotations.Test;
+
+public class JenkinsPgm 
+{
+	@Test
+	public void hello()
+	{
+		System.out.println("hello jenkins");
+	}
+	
+	@Test
+	public void methodTwo()
+	{
+		System.out.println("Good Morning");
+	}
+
+	@Test
+	public void jenkinsThree()
+	{
+		System.out.println("hi");
+	}
+	
+}
